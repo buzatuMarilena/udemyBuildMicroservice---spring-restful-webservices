e05914c0cd3f992935fd16c2616b90690e561c51
buzatuMarilena <buzatumarilena@gmail.com>
2024-11-12 16:22:42 +0200

    section 3 and section 4

---
+++ src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package net.javaguides.springboot.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.mapper.UserMapper;
+import net.javaguides.springboot.repository.UserRepository;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        // Convert UserDto info user jpa entity
+        User user = UserMapper.mapToUser(userDto);
+        User savedUser = userRepository.save(user);
+
+        // Convert user jpa entity into UserDto
+        return UserMapper.mapToUserDto(savedUser);
+    }
+
+    @Override
+    public UserDto getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        User user = optionalUser.get();
+        return UserMapper.mapToUserDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users =  userRepository.findAll();
+        return users.stream().map(UserMapper::mapToUserDto).toList();
+    }
+
+    @Override
+    public UserDto updateUser(UserDto user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = userRepository.save(existingUser);
+        return UserMapper.mapToUserDto(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
